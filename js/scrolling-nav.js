faa6d2d16e803f6b2c08c6888ff7f01fb98479ca
Liam Dungan <liamdungan@yahoo.com>
2018-02-20 20:17:00 -0500

    fonts

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,7 +1,26 @@
 $(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-     }, 700);
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 700);
+});
+
+
+
+//show nav bar when scrolled 50px down
+$(document).ready(function() {
+	var dist = 50;
+	$(window).scroll(function () {
+		var scrollTop = $(window).scrollTop();
+		if (scrollTop > dist) {
+			if (($("#nav").is(":visible") === false)) {
+				$('#nav').fadeIn('slow');
+			}
+		} else {
+			if ($("#nav").is(":visible")) {
+				$('#nav').hide();
+			}
+		}
+	});
 });
